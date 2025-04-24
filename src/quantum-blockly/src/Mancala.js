a2f9256abfcaae966021b3915eabbdf69b38f891
RobertDuncan1 <duncanrm2@vcu.edu>
2025-04-24 19:14:12 -0400

    push

--- src/quantum-blockly/src/Mancala.js
+++ src/quantum-blockly/src/Mancala.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Mancala';
+import './Mancala.css';
 
 const Mancala = ({ quboCode, log }) => {
   const [gameSetup, setGameSetup] = useState(false);
